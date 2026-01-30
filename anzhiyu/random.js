025a5ab0ea183ad7598e03e81e9851b377d274db
OierWJX <3773068575@qq.com>
2026-01-30 13:20:03 +0800

    Site updated: 2026-01-30 13:19:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/29/CSP2025游记/","2026/01/29/各种代码板子/"];function toRandomPost(){
+var posts=["2026/01/29/CSP2025游记/","2026/01/29/各种代码板子/","2026/01/30/如何搭建个人博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
